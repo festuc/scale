3d0654ff54947a105b2f2daf84799f1a64b93945
Eduard Vidal Tulsà <64154935+festuc@users.noreply.github.com>
2021-02-13 08:59:11 +0100

    Update SingInAtWork.php

--- SingInAtWork.php
+++ SingInAtWork.php
@@ -1,13 +1,10 @@
 <?php
 require("login.php");
-// pre($_REQUEST);
-// die;
 if($_REQUEST['singkey']==$singkey)$login=true;
 $_REQUEST['id']=urlencode($_REQUEST['id']);
-if ($login==false){$link->close();header("Location: {$scale_uri}");$link->close();die;}
+if ($login==false){$link->close();header("Location: {$scale_uri}");die;}
 mysql_quer("INSERT INTO `SingInAtWork` (`ID`,`ID_Empresa`,`diahora`)
 VALUES ('{$_REQUEST['id']}', '{$_REQUEST['ID_Empresa']}', now());",$link);
 $scale_uri=urldecode($_REQUEST['return_uri']);
-// a($scale_uri);die;
 $link->close();header("Location: {$scale_uri}");die;
 ?>
